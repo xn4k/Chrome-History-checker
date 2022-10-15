d3c1a88b96d9936117947b41483aa0a4d539843e
xn4k <stueckholz@hotmail.com>
2022-10-15 02:42:51 +0200

    irgendwas

--- main.go
+++ main.go
@@ -7,6 +7,32 @@ import (
 	"log"
 )
 
+func execRow() {
+	db, err := sql.Open("sqlite3", "test.db")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	sts := `
+		SELECT 
+		  datetime(last_visit_time/1000000-11644473600, "unixepoch") as last_visited, 
+		  url, 
+		  title, 
+		  visit_count 
+		FROM urls
+		`
+	_, err = db.Exec(sts)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("table cars created")
+}
+
 // checkSqlite3Version checks what version of sqlite3 is installed on the host.
 func checkSqlite3Version() {
 	db, err := sql.Open("sqlite3", ":memory:")
